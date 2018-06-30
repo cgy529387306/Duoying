0c35b2d041c829ed96d2a955a48fce939ff5a1d6
caigy <cai_guiyou@163.com>
2018-06-30 23:39:56 +0800

    init

---
+++ app/src/main/java/com/mb/duoying/MainActivity.java
@@ -0,0 +1,73 @@
+package com.mb.duoying;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private String myUrl = "https://www.dy866.com";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl(myUrl);//加载url
+        webView.setWebViewClient(webViewClient);
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);//允许使用js
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+    }
+
+    //WebViewClient主要帮助WebView处理各种通知、请求事件
+    private WebViewClient webViewClient=new WebViewClient(){
+        @Override
+        public void onPageFinished(WebView view, String url) {//页面加载完成
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {//页面开始加载
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.i("ansen","拦截url:"+url);
+            if(url.equals("http://www.google.com/")){
+                Toast.makeText(MainActivity.this,"国内不能访问google,拦截该url",Toast.LENGTH_LONG).show();
+                return true;//表示我已经处理过了
+            }
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+    };
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //释放资源
+        webView.destroy();
+        webView = null;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) ) {
+            if (webView.canGoBack()) {
+                webView.goBack(); //goBack()表示返回WebView的上一页面
+                return true;
+            }else {
+                finish();
+                return true;
+            }
+        }
+        return false;
+    }
+}
